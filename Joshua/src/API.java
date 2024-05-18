d8825e5e0e0419f44432ecda3e7ea57b0df4408f
bruceroberts <bruceroberts@android-4bc2cc9cfcbe4378.localdomain>
2024-05-18 13:09:40 -0400

    test

--- Joshua/src/API.java
+++ Joshua/src/API.java
@@ -1,6 +1,9 @@
 
 public class API {
 
+	private static String CLIENT_ID = "bebcef28ebfd41549408ad18eb638059";
+	private static String CLIENT_SECRET = "63834c8901ed4008886a25691e790bc7";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
