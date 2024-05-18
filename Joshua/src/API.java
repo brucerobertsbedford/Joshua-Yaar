68171aac04f8dab1db7bd83aa89c8ec068d661cb
bruceroberts <bruceroberts@android-4bc2cc9cfcbe4378.localdomain>
2024-05-18 14:11:05 -0400

    Added json parsing to get access token

--- Joshua/src/API.java
+++ Joshua/src/API.java
@@ -1,27 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.io.*;
-import java.net.*;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class API {
 
 	private static String CLIENT_ID = "bebcef28ebfd41549408ad18eb638059";
 	private static String CLIENT_SECRET = "63834c8901ed4008886a25691e790bc7";
 	private static String TOKEN_URL = "https://accounts.spotify.com/api/token";
-	private static String PARAMS = "grant_type=client_credentials&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET;
+	private static String ACCESS_TOKEN_NAME = "access_token";
 	
 	public static void main(String[] args) {
 		sendPost(TOKEN_URL, CLIENT_ID, CLIENT_SECRET);
 
 	}
 
-	private static void sendPost(String url, String client_id, String client_secret) {
+	private static String sendPost(String url, String client_id, String client_secret) {
 		try {
+			String params = "grant_type=client_credentials&client_id=" + client_id + "&client_secret=" + client_secret;
 			URL obj = new URL(url);
 			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 			con.setRequestMethod("POST");
 			con.setDoOutput(true);
 			OutputStream os = con.getOutputStream();
-			os.write(PARAMS.getBytes());
+			os.write(params.getBytes());
 			os.flush();
 			os.close();
 			
@@ -38,8 +45,12 @@ public class API {
 				in.close();
 				System.out.println(response);
 				
+				String accessToken = getJsonMember(ACCESS_TOKEN_NAME, response.toString());		
+				System.out.println("accessToken:  " + accessToken);
+				return accessToken;
 			} else {
 				System.out.println("Post returned HTTP error code:  " + responseCode);
+				return "";
 			}
 			
 		} catch (MalformedURLException e) {
@@ -49,7 +60,19 @@ public class API {
 			System.out.println("Error opening connection to url:  " + url);
 			e.printStackTrace();
 		}
+		return "";
 		
+	}
+	
+	private static String getJsonMember(String memberName, String json) {
+		String result = "";
+		String regex = "[.]*\"" + memberName + "\":\"(.+?)\"";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(json);
+		if (matcher.find()) {
+			result = matcher.group(1);
+		}
+		return result;
 	}
 
 }
